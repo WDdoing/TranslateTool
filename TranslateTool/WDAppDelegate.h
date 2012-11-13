24c84cab27f93a3569d5d479fea3056ef5d40d53
wang <wang@wangtekiMacBook-Pro.local>
2012-11-13 21:14:43 +0800

    Initial Commit

---
+++ TranslateTool/WDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDAppDelegate.h
+//  TranslateTool
+//
+//  Created by wang on 12-11-13.
+//  Copyright (c) 2012年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TranslateTool/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TranslateTool
+//
+//  Created by wang on 12-11-13.
+//  Copyright (c) 2012年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WDAppDelegate class]));
+    }
+}
